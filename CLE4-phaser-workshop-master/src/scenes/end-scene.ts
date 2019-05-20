cff663c7496e2c463bbee45cd64d961acecac808
FernandoFernandezCepero <43050422+FernandoFernandezCepero@users.noreply.github.com>
2019-05-20 13:53:48 +0200

    Eerste prototype
    Dit is een prototype gemaakt aan de hand van de workshop van 8-5-2019.

---
+++ CLE4-phaser-workshop-master/src/scenes/end-scene.ts
@@ -0,0 +1,28 @@
+export class EndScene extends Phaser.Scene {
+
+    constructor() {
+        super({key: "EndScene"})
+    }
+
+    init(): void {
+    }
+
+    preload(): void {
+    }
+
+    create(): void {
+        // change this to a nice game over image
+
+        this.add.image(0, 0, 'sky').setOrigin(0, 0)
+
+        // add text here
+        this.add.text(650, 300, "Game Over", { fontFamily: 'Arial Black', fontSize: 70, color: '#2ac9be' }).setOrigin(0.5).setStroke('#7df2ea', 16)
+
+        this.add.text(650, 400, 'Click here to Restart', { fontFamily: 'Arial Black', fontSize: 35, color: 'grey' }).setOrigin(0.5).setStroke('black', 5)
+
+        // add code here to switch to the GameScene, after a mouse click
+        this.input.once('pointerdown', (pointer) => {
+            this.scene.start('GameScene')
+        })
+    }
+}
---
+++ CLE4-phaser-workshop-master/src/scenes/game-scene.ts
@@ -0,0 +1,98 @@
+import { Player } from "../objects/player"
+import { Platform } from "../objects/platform"
+import { MovingPlatform } from "../objects/movingplatform"
+import { Bomb } from "../objects/bomb"
+import { water} from "../objects/water"
+import { Cameras } from "phaser";
+import { platform } from "os";
+
+export class GameScene extends Phaser.Scene {
+    
+    private player : Player
+    private platforms: Phaser.GameObjects.Group
+    private stars: Phaser.Physics.Arcade.Group
+    private bombs: Phaser.GameObjects.Group
+    private water: Phaser.GameObjects.Group
+    private collectedBanana = 0
+    private scoreField
+
+    constructor() {
+        super({ key: "GameScene" })
+    }
+
+    init(): void {
+        console.log("dit is een gamescene")
+        this.registry.values.score = 0
+    }
+
+    create(): void {
+        this.add.image(0, 0, 'sky').setOrigin(0, 0)      
+        
+        // 11 STARS
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 30, stepX: 70 },
+        })
+
+        this.bombs = this.add.group()
+        this.bombs.add(new Bomb(this, 20, 20), true)
+        this.bombs.add(new Bomb(this, 120, 20), true)
+
+        // TODO add player
+        this.player = new Player(this)
+
+        this.platforms = this.add.group({ runChildUpdate: true })
+        this.platforms.addMultiple([
+            new Platform(this, 110, 574, "ground"),
+            new Platform(this, 1200, 574, "ground1"),
+            new Platform(this, 500, 350, "ice"),
+            new Platform(this, 250, 450, "platform"),
+            new MovingPlatform(this, 100, 250, "platform"),
+        ], true)
+        
+        this.scoreField = this.add.text(250, 20, this.collectedBanana + ' Bananas collected', { fontFamily: 'Arial Black', fontSize: 40, color: '#2ac9be' }).setOrigin(0.5).setStroke('#000000', 5)
+        // define collisions for bouncing, and overlaps for pickups
+        this.physics.add.collider(this.stars, this.platforms)
+        this.physics.add.collider(this.bombs, this.platforms)
+        this.physics.add.collider(this.player, this.platforms)
+        
+        this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this)
+        this.physics.add.overlap(this.player, this.bombs, this.hitBomb, null, this)
+        this.physics.add.overlap(this.player, this.water, this.hitWater, null, this)
+
+        this.physics.world.bounds.width = 1600
+        this.physics.world.bounds.height = 600
+        
+        this.cameras.main.setSize(1440, 900)
+        this.cameras.main.setBounds(0, 0, 1600, 600)
+        this.cameras.main.startFollow(this.player)
+    }
+
+    private hitBomb(player:Player, bomb){
+        this.bombs.remove(bomb, true, true)
+        this.scene.start('EndScene')
+    }
+
+    private hitWater(player:Player, water){
+        
+        this.scene.start('EndScene')
+    }
+
+    private collectStar(player : Player , star) : void {
+        this.stars.remove(star, true, true)
+        this.registry.values.score++
+        this.collectedBanana++
+        console.log(this.registry.values.score + ' sterren')
+
+        // TO DO check if we have all the stars, then go to the end scene'
+        this.scoreField.text = this.collectedBanana + ' Bananas collected'
+    
+    }
+
+    update(){
+        this.player.update()
+        // this.add.text(170, 50,' Sterren', { fontFamily: 'Arial Black', fontSize: 40, color: '#2ac9be' }).setOrigin(0.5).setStroke('#000000', 5)
+    }
+
+}
